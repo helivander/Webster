aca296e0bfdb453e19aea59e03881721bb1ee292
Helivander Lima <helivander@gmail.com>
2025-05-05 22:23:59 -0300

    fix: corrigir uso do hook 'beforeExit' no PrismaService

--- server/src/shared/services/prisma.service.ts
+++ server/src/shared/services/prisma.service.ts
@@ -15,7 +15,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   }
 
   async enableShutdownHooks(app: INestApplication): Promise<void> {
-    this.$on('beforeExit', async () => {
+    process.on('beforeExit', async () => {
       await app.close();
     });
   }
